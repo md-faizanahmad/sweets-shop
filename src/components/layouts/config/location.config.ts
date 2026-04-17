85a4466b855ac12923c35c4731f1aef34d6540c8
Md Faizan Ahmad <mdfaizan7563@gmail.com>
2026-04-17 18:29:15 +0530

    feat: responsive location.tsx

--- src/components/layouts/config/location.config.ts
+++ src/components/layouts/config/location.config.ts
@@ -7,7 +7,7 @@ export type LocationConfig = {
 export const locationConfig: LocationConfig = {
   // CORRECT EMBED FORMAT
   embedUrl:
-    "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d57904.38283626151!2d84.970104!3d24.7975!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39f32a440a370e83%3A0x40f3759494793740!2sGaya%2C%20Bihar!5e0!3m2!1sen!2sin!4v1712850000000!5m2!1sen!2sin",
+    "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d47392.47186792881!2d84.94079496870911!3d24.78331058896528!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39f32a440a1b3c1f%3A0xcef6b223bdbf34a6!2sGaya%2C%20Bihar!5e1!3m2!1sen!2sin!4v1776430570215!5m2!1sen!2sin",
 
   // Directions link for mobile/button
   directionsUrl: "https://www.google.com/maps/dir//Gaya,+Bihar",
