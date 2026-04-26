c3df3bb7967a146403e8d229eca237d709274d3d
Md Faizan Ahmad <mdfaizan7563@gmail.com>
2026-04-26 19:31:33 +0530

    feat: public/manifest.json

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
-  const baseUrl = "https://yourdomain.com";
+  const baseUrl = "https://sweets-shop-theta.vercel.app";
 
   return {
     rules: {
